f0686e1201ed918dc0ec3419d976dd38010f3830
Brijesh Pant <bpant@xebia.com>
2020-05-25 08:56:30 +0530

    Added typedef

--- src/product.js
+++ src/product.js
@@ -4,7 +4,7 @@ export const ProductType = new GraphQLObjectType({
     name: "Product",
     description: "Product type",
     fields: () =>({
-        id: {type: GraphQLString},
+        id: { type: GraphQLString},
         name: {type: GraphQLString},
         brand: {type: GraphQLString},
         category: {type: GraphQLString},
